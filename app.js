5d97b64ee4e8b76c24b322f22ba8adbf482c3a06
carotene512 <karoten512@gmail.com>
2018-04-12 23:03:26 +0900

    modalを開く処理と閉じる処理をリファクタリング

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+/** このかきかただと１つのmodalにしかたいおうできないんだよなぁ */
+
 function Modal(el) {
   this.$el = el;
-
   /** overlayの取得 */
-  this.$overlay    = $('#modal-overlay');
+  this.$overlay    = $('#modal_overlay');
   /** modal本体の取得 */
   this.$modal_body = $('#modal');
+  /** 閉じるボタンの取得 */
+  this.$close_trigger = $('#close_trigger');
   /** イベントハンドラの設定 */
   this.handleEvents();
 }
@@ -12,21 +15,32 @@ function Modal(el) {
 Modal.prototype.handleEvents = function() {
   const self = this;
 
+  /** triggerがclickされたらmodal画面を開く */
   this.$el.on('click', function(e) {
-
-    /** overlayの表示 */
-    self.$overlay.fadeIn();
-
-    /** modal本体の表示 */
-    self.$modal_body.fadeIn();
-    // console.log(this); // イベントが発生したjavascriptのdom要素
+    self.show();
   });
 
   /** overlayがclickされたら画面を閉じる */
   this.$overlay.on('click', function() {
-    self.$overlay.fadeOut();
-    self.$modal_body.fadeOut();
+    self.hide();
+  });
+
+  /** closeボタンがclickされたら画面を閉じる */
+  this.$close_trigger.on('click', function() {
+    self.hide();
   });
 }
 
-const modal = new Modal($("#modal-trigger"));
+/** modal表示処理 */
+Modal.prototype.show = function() {
+  this.$overlay.fadeIn();
+  this.$modal_body.fadeIn();
+}
+
+/** modal消去処理 */
+Modal.prototype.hide = function() {
+  this.$overlay.fadeOut();
+  this.$modal_body.fadeOut();
+}
+
+const modal = new Modal($("#modal_trigger"));
