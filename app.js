b6fcf8d40f2f476fedcb15b07178c75a97cf1ed7
carotene512 <karoten512@gmail.com>
2018-04-12 22:39:48 +0900

    modal本体も一旦表示。

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 function Modal(el) {
   this.$el = el;
+
   /** overlayの取得 */
-  this.$overlay = $('#modal-overlay');
+  this.$overlay    = $('#modal-overlay');
+  /** modal本体の取得 */
+  this.$modal_body = $('#modal');
+  /** イベントハンドラの設定 */
   this.handleEvents();
 }
 
@@ -9,7 +13,13 @@ Modal.prototype.handleEvents = function() {
   const self = this;
 
   this.$el.on('click', function(e) {
+    /** overlayの表示 */
     self.$overlay.fadeIn();
+
+    /** modal本体の表示 */
+    console.log(self.$modal_body);
+    self.$modal_body.fadeIn();
+
     // console.log(this); // イベントが発生したjavascriptのdom要素
   });
 }
