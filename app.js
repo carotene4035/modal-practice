c92c3013da5519ced14a4eeac806d0a996e00e8b
carotene512 <karoten512@gmail.com>
2018-04-21 10:42:43 +0900

    変更されやすい項目を定数として切りだした。

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 (function() {
 
   var Modal = function() {
-
-    /** clickされたときのイベントを登録 */
-    $(document).on('click', '[data-toggle]', function() {
-
-      /** modalのターゲットを取得 */
-      const targetModalId = $(this).data('target');
-      const $targetModal = $(targetModalId);
-
-      /** modalを表示 */
-      $targetModal.css('display', 'block');
-      $targetModal.addClass('show');
-
-      /** overlayを動的に生成して、表示する */
-      const $overlay = $(document.createElement('div'));
-      $overlay.addClass('modal-backdrop');
-      $overlay.css('display', 'block');
-      $overlay.appendTo(document.body);
-      $overlay.addClass('show');
-    });
+    /** 抽象化されたmodalオブジェクト: 各modalの情報を抽象化して、まとめて扱えるようにする */
 
   }();
+
+  var Selector = {
+    BACKDROP: 'modal-backdrop',
+    DATA_TOGGLE: '[data-toggle="modal"]'
+    MODAL_TARGET: 'target'
+  };
+
+  var ClassName = {
+    FADE: 'fade',
+    SHOW: 'show'
+  };
+
+  /** clickされたときのイベントを登録 */
+  $(document).on('click', Selector.DATA_TOGGLE, function() {
+
+    /** modalのターゲットを取得 */
+    const targetModalId = $(this).data('target');
+    const $targetModal = $(targetModalId);
+
+    /** modalを表示 */
+    $targetModal.css('display', 'block');
+    $targetModal.addClass(ClassName.SHOW);
+
+    /** overlayを動的に生成して、表示する */
+    const $backdrop = $(document.createElement('div'));
+    $backdrop.addClass(Selector.BACKDROP);
+    $backdrop.css('display', 'block');
+    $backdrop.appendTo(document.body);
+    $backdrop.addClass(ClassName.SHOW);
+  });
+  /** 動く、、、そして、複数のmodalにも対応できている。これは何に困るんだろう */
+  /** 再利用性が低い */
 
 })();
