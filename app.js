aebb061273a2411a21c7dd964ddee4897639605c
carotene512 <karoten512@gmail.com>
2018-04-21 10:28:16 +0900

    処理をclosureの中にまとめて、変数のスコープを狭めた。

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 (function() {
-  /** clickされたときのイベントを登録 */
-  $(document).on('click', '[data-toggle]', function() {
 
-    /** modalのターゲットを取得 */
-    const targetModalId = $(this).data('target');
-    const $targetModal = $(targetModalId);
+  var Modal = function() {
 
-    /** modalを表示 */
-    $targetModal.css('display', 'block');
-    $targetModal.addClass('show');
+    /** clickされたときのイベントを登録 */
+    $(document).on('click', '[data-toggle]', function() {
 
-    /** overlayを動的に生成して、表示する */
-    const $overlay = $(document.createElement('div'));
-    $overlay.addClass('modal-backdrop');
-    $overlay.css('display', 'block');
-    $overlay.appendTo(document.body);
-    $overlay.addClass('show');
-  });
+      /** modalのターゲットを取得 */
+      const targetModalId = $(this).data('target');
+      const $targetModal = $(targetModalId);
+
+      /** modalを表示 */
+      $targetModal.css('display', 'block');
+      $targetModal.addClass('show');
+
+      /** overlayを動的に生成して、表示する */
+      const $overlay = $(document.createElement('div'));
+      $overlay.addClass('modal-backdrop');
+      $overlay.css('display', 'block');
+      $overlay.appendTo(document.body);
+      $overlay.addClass('show');
+    });
+
+  }();
 
 })();
