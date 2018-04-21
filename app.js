640aaa1e5680ff1974f89faff34301362a4d4b8d
carotene512 <karoten512@gmail.com>
2018-04-21 16:04:55 +0900

    modalクラスの生成と、次にやりたいことを記述。

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 (function() {
 
-  var Modal = function() {
-    /** 抽象化されたmodalオブジェクト: 各modalの情報を抽象化して、まとめて扱えるようにする */
-
-  }();
-
+  /** private */
   var Selector = {
     BACKDROP: 'modal-backdrop',
     DATA_TOGGLE: '[data-toggle="modal"]'
@@ -15,12 +11,34 @@
     SHOW: 'show'
   };
 
+  var Modal = function() {
+    /** クラス */
+    function Modal() {
+      this._isShown = false;
+    };
+    Modal.prototype.show = function() {
+      console.log('見せるよ');
+    }
+    /** クラスを返している */
+    return Modal;
+  }();
+
+  console.log(Modal);
+  console.log(new Modal); // これでオブジェクトになる
+
+
   /** clickされたときのイベントを登録 */
   $(document).on('click', Selector.DATA_TOGGLE, function() {
 
     /** modalのターゲットを取得 */
     const targetModalId = $(this).data('target');
     const $targetModal = $(targetModalId);
+
+    /** やりたいこと */
+
+    /** modalの操作を、ここ（イベント定義）から追い出したい */
+    /** modalオブジェクト内に収めたい */
+
 
     /** modalを表示 */
     $targetModal.css('display', 'block');
@@ -33,6 +51,8 @@
     $backdrop.appendTo(document.body);
     $backdrop.addClass(ClassName.SHOW);
   });
+
+
   /** 動く、、、そして、複数のmodalにも対応できている。これは何に困るんだろう */
   /** 再利用性が低い */
 
